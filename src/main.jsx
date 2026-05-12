e36cb49e36bbb401e208b81a56a61c77609c4580
Sofia Giraldo Sanchez <sgiraldos1@eafit.edu.co>
2026-05-11 20:29:28 -0500

    vite and main.jsx fix

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import Checkout from "./components/organisms/checkout/Checkout"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Navigate to="/gallery" replace />} />
